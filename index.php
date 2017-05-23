176a560038e9d50535637891d5c8004075f7208a
sisdev <sisdev@sisoft.in>
2017-05-23 18:54:22 +0530

    left on 23rd may
    left - right one

--- index.php
+++ index.php
@@ -2,38 +2,6 @@
 require('connection.php');
 session_start();	
 
-/*
-if(isset($_POST['login']))
-{
-	$email=$_POST['email'];
-	$pass=$_POST['pass'];
-if(isset($_POST['remember']))
-{
-	$cookie_name = "user";
-$cookie_value = $email;
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-}
-	
-	$record=mysqli_query("select userid,username,userpass,role from users where username='$email' and userpass='$pass' ");
-	$name=mysql_fetch_array($record);
-	$count=mysql_num_rows($record);
-	if($count==1)
-	{	
-		
-	$_SESSION['userid']=$name['userid'];
-	$_SESSION['email']=$name['username'];
-	header("location:teacher-page.php");
-		
-	
-	}
-	else
-	{
-		$error="<font color=red>Email or Password incorrect!</font>";
-		
-	}
-}
-*/
-
 if(isset($_GET['session']))
 {
 unset($_SESSION['login']);	
@@ -42,24 +10,24 @@ unset($_SESSION['login']);
 }
 ?>
 
-
-<?php include('header-2.php'); ?>
-<?php include('login_reg.php'); ?>
-
 <html lang="en">
 <head>
+  <title>Online Exam/Assessment Web - Sisoft</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Online Exam/Assessment System by Sisoft">
   <meta name="author" content="Sisoft Learning">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.js" type="text/javascript"></script>
-  <title>Assessment Web</title>
+
+  <link rel="stylesheet" href="css/bootstrap.css">
+	
+  <script  type="text/javascript" src="js/jquery-3.2.1.min.js"> </script>
+  <script type="text/javascript"  src="js/bootstrap.min.js" ></script>
+ 
   </head>
   <body>
   
+<?php include('header-2.php'); ?>
+<?php include('login_reg.php'); ?>
 
 
   <div class="container">
@@ -70,9 +38,9 @@ unset($_SESSION['login']);
 	  <div class="col-sm-3"></div>
 	  <div class="col-sm-6"><img src="images/login.jpg" class="img-responsive img-rounded"></img></div>
 	  <div class="col-sm-3">
-	    
-		<a href="teacher-page.php">Exam - Home </a>
-	   
+	  
+	  <a href="teacher-page.php">Exam - Home </a>
+	  
 	  </div>
  
 
